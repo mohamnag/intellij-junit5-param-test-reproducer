9c0a2a82c613a87d167980c40553c087b0093078
Mohammad Naghavi <mohamnag@gmail.com>
2021-04-14 18:00:51 +0200

    use generated sample and a simple test

---
+++ src/test/java/com/example/starter/TestMainVerticle.java
@@ -0,0 +1,27 @@
+package com.example.starter;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  void verticle_deployed(VertxTestContext testContext) {
+    testContext.completeNow();
+  }
+
+  @Test
+  void whatever() {
+    System.out.println("yuhu!");
+  }
+}
